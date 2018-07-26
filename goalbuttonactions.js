ccb29410bb5c88086852341bfc73540e2bbb41e6
kieranwhooley <kieranwhooley@gmail.com>
2018-07-26 13:25:57 -0400

    Added Yes and No buttons to grid

---
+++ goalbuttonactions.js
@@ -0,0 +1,31 @@
+document.getElementById('goalsuccessday1').onclick = function() {
+   var no = document.getElementById('goalnotsuccessday1');
+    no.style.display = "none";
+};
+
+document.getElementById('goalnotsuccessday1').onclick = function() {
+   var no = document.getElementById('goalsuccessday1');
+    no.style.display = "none";
+};
+
+document.getElementById('goalsuccessday2').onclick = function() {
+   var no = document.getElementById('goalnotsuccessday2');
+    no.style.display = "none";
+};
+
+document.getElementById('goalnotsuccessday2').onclick = function() {
+   var no = document.getElementById('goalsuccessday2');
+    no.style.display = "none";
+};
+
+document.getElementById('goalsuccessday3').onclick = function() {
+   var no = document.getElementById('goalnotsuccessday3');
+    no.style.display = "none";
+};
+
+document.getElementById('goalnotsuccessday3').onclick = function() {
+   var no = document.getElementById('goalsuccessday3');
+    no.style.display = "none";
+};
+
+
