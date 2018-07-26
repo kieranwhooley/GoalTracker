cf9752eff7c1d1326661d5c68b920498438d1491
kieranwhooley <kieranwhooley@gmail.com>
2018-07-26 15:58:06 -0400

    Added more buttons to grid, updated 7grid link

--- goalbuttonactions.js
+++ goalbuttonactions.js
@@ -28,4 +28,42 @@ document.getElementById('goalnotsuccessday3').onclick = function() {
     no.style.display = "none";
 };
 
+document.getElementById('goalsuccessday4').onclick = function() {
+   var no = document.getElementById('goalnotsuccessday4');
+    no.style.display = "none";
+};
 
+document.getElementById('goalnotsuccessday4').onclick = function() {
+   var no = document.getElementById('goalsuccessday4');
+    no.style.display = "none";
+};
+
+document.getElementById('goalsuccessday5').onclick = function() {
+   var no = document.getElementById('goalnotsuccessday5');
+    no.style.display = "none";
+};
+
+document.getElementById('goalnotsuccessday5').onclick = function() {
+   var no = document.getElementById('goalsuccessday5');
+    no.style.display = "none";
+};
+
+document.getElementById('goalsuccessday6').onclick = function() {
+   var no = document.getElementById('goalnotsuccessday6');
+    no.style.display = "none";
+};
+
+document.getElementById('goalnotsuccessday6').onclick = function() {
+   var no = document.getElementById('goalsuccessday6');
+    no.style.display = "none";
+};
+
+document.getElementById('goalsuccessday7').onclick = function() {
+   var no = document.getElementById('goalnotsuccessday7');
+    no.style.display = "none";
+};
+
+document.getElementById('goalnotsuccessday7').onclick = function() {
+   var no = document.getElementById('goalsuccessday7');
+    no.style.display = "none";
+};
